0259597d503391131a4c5653df5eebe832163048
Chala-java <chalajuandavid876@gmail.com>
2025-04-09 07:59:07 -0500

    implemente el corse para conectar Front

--- Backend/src/main/java/com/example/E_commerce_chala/repository/IDProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.E_commerce_chala.repository;
-
-import com.example.E_commerce_chala.models.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IDProductoRepository extends JpaRepository <Producto,Integer> {
-
-}
